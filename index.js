9adbe41c8e4dafaaf7069dcf5fd56e9631065c34
Akshay8788 <akshaymahajan5579@gmail.com>
2025-04-14 21:49:54 -0700

    Added CORS

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ intializeDatabase()
 const Books = require("./model")
 const express = require("express")
 const app = express()
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json())
 
